8cd2890b0934bef22b0c108216672a7dc18dfe56
Fernando De Jesus <fdejesus95@outlook.com>
2024-05-04 13:54:53 -0400

    added images to json file and changed the GET request from parents to inside child component 'SneakerPage'

--- src/component/App.js
+++ src/component/App.js
@@ -1,24 +1,14 @@
-import React, {useEffect, useState} from "react"
+import React from "react"
 // import './App.css'; 
 import Header from "./Header"
 import SneakerPage from "./SneakerPage"
 
 function App() {
-  const [sneakers, setSneakers] = useState([])
-
-  useEffect(() =>{
-    fetch ('http://localhost:3000')
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data)
-      setSneakers(data)
-    })
-  }, [])
 
   return (
     <div className="App"> 
-    <Header classname="App-header"/>
-    <SneakerPage sneakers={sneakers}/>
+      <Header classname="App-header"/>
+      <SneakerPage />
     </div>
   );
 }
--- src/component/SneakerPage.js
+++ src/component/SneakerPage.js
@@ -1,9 +1,20 @@
-import React,{useState} from "react" 
+import React,{useState, useEffect} from "react" 
 import Search from "./Search"
 
-function SneakerPage({sneakers}) { 
+function SneakerPage() { 
     const [sneakers, setSneakers] = useState([])
     const [searchSneaker,setSearchSneaker]= useState("") 
+
+    useEffect(() =>{
+        fetch("http://localhost:3000/sneakers")
+        .then((resp) => resp.json())
+        .then((data) => {
+          console.log(data)
+          setSneakers(data)
+        })
+    }, [])
+
+
     return( 
         <main> 
 
