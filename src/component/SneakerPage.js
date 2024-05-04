fabffc7b3a6ed1eaaaac114858ed85dd59ee2792
Fernando De Jesus <fdejesus95@outlook.com>
2024-05-04 14:13:47 -0400

    created varialbe 'addSneaker' and POST method

--- src/component/SneakerPage.js
+++ src/component/SneakerPage.js
@@ -1,9 +1,23 @@
 import React,{useState, useEffect} from "react" 
-import Search from "./Search"
+import NewSneakerForm from "./NewSneakerForm"
+import SneakerList from "./SneakerList"
+import SneakerCard from "./SneakerCard"
 
 function SneakerPage() { 
     const [sneakers, setSneakers] = useState([])
     const [searchSneaker,setSearchSneaker]= useState("") 
+
+    const addSneaker = (newSneaker) => {
+        fetch("http://localhost:3000/sneakers", {
+            method: "POST",
+            headers: {
+                "Content-type": "Application/json"
+            },
+            body: JSON.stringify(newSneaker)
+        })
+        .then((resp)=> resp.json())
+        .then((data)=> setSneakers([...sneakers, data]))
+    }
 
     useEffect(() =>{
         fetch("http://localhost:3000/sneakers")
@@ -17,7 +31,9 @@ function SneakerPage() {
 
     return( 
         <main> 
-
+            <NewSneakerForm addSneaker={addSneaker}/>
+            <SneakerList />
+            <SneakerCard />
         </main>
     )
 } 
