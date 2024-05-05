7d1e054272d3bef59201bdcc426d91710c1cec8b
Joel John <johnjoel079@gmail.com>
2024-05-05 14:09:04 -0400

    updated SneakerPage and search component

--- src/component/SneakerPage.js
+++ src/component/SneakerPage.js
@@ -1,7 +1,8 @@
 import React,{useState, useEffect} from "react" 
 import NewSneakerForm from "./NewSneakerForm"
 import SneakerList from "./SneakerList"
-import SneakerCard from "./SneakerCard"
+import SneakerCard from "./SneakerCard" 
+import Search from "./Search"
 
 function SneakerPage() { 
     const [sneakers, setSneakers] = useState([])
@@ -17,7 +18,11 @@ function SneakerPage() {
         })
         .then((resp)=> resp.json())
         .then((data)=> setSneakers([...sneakers, data]))
-    }
+    } 
+
+    const filtered = sneakers.filter((sneaker) => { 
+        return sneaker.name.toLowerCase().includes(searchSneaker.toLowerCase())
+    })
 
     useEffect(() =>{
         fetch("http://localhost:3000/sneakers")
@@ -32,8 +37,9 @@ function SneakerPage() {
     return( 
         <main> 
             <NewSneakerForm addSneaker={addSneaker}/>
-            <SneakerList />
-            <SneakerCard />
+            <SneakerList sneakers={filtered}/>
+            <SneakerCard /> 
+            <Search searchSneaker={searchSneaker} setSearchSneaker={setSearchSneaker}/>
         </main>
     )
 } 
