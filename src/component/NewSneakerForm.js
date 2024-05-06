93bb977b0a2238d1ff667d26fb30d495127a75b9
Joel John <johnjoel079@gmail.com>
2024-05-05 20:34:49 -0400

    NewSneakerForm updated

--- src/component/NewSneakerForm.js
+++ src/component/NewSneakerForm.js
@@ -1,7 +1,14 @@
-import React from "react" 
+import React, {useState} from "react" 
 
 function NewSneakerForm({addSneaker}) { 
+    const[newSneaker,setNewSneaker] = useState({ 
+        name: '', 
+        image:'', 
+        retailprice:'', 
+        resellprice:''
+    })
 
 } 
 
-export default NewSneakerForm; 
+export default NewSneakerForm;  
+
