75d6e78e3e81c654b3d534f6a9be5341757b7672
Fernando De Jesus <fdejesus95@outlook.com>
2024-05-06 15:32:34 -0400

    added more sizes

--- src/component/SneakerSize.js
+++ src/component/SneakerSize.js
@@ -8,14 +8,23 @@ function SneakerSize({ setSneakerSize }) {
             onChange={(e) => setSneakerSize(e.target.value)}>
             <option>All</option>
             <option>4</option>
+            <option>4.5</option>
             <option>5</option>
+            <option>5.5</option>
             <option>6</option>
+            <option>6.5</option>
             <option>7</option>
+            <option>7.5</option>
             <option>8</option>
+            <option>8.5</option>
             <option>9</option>
+            <option>9.5</option>
             <option>10</option>
+            <option>10.5</option>
             <option>11</option>
+            <option>11.5</option>
             <option>12</option>
+            <option>12.5</option>
             <option>13</option>
         </select>
     )
