69e1fe557338ad6ce5f06c8230e8b091d8f883c3
Joel John <johnjoel079@gmail.com>
2024-05-07 20:26:36 -0400

    made error page component

---
+++ src/component/ErrorPage.js
@@ -0,0 +1,19 @@
+import { useRouteError } from "react-router-dom"; 
+
+function ErrorPage () { 
+    const error = useRouteError(); 
+    console.error(error) 
+
+  return( 
+    <>
+      <header>
+        Hey!
+      </header>
+      <main>
+        <h1>Oops! Looks like something went wrong.</h1>
+      </main>
+    </>
+  );
+} 
+
+export default ErrorPage;
